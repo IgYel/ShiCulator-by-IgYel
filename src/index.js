f9dfff608cfebd6b8038c8443cfaeda652ffb5ff
IgYel <cutelyfoxy@gmail.com>
2025-01-17 19:31:02 +0100

    fixes of rote for pages

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./scss/_index.scss";
 import { App } from "./App";
 import { Components } from "./components/Components";
-import { BrowserRouter } from "react-router-dom";
 import { RouteComponent } from "./routes/RoutesComponent";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <App Components={Components} RouteComponent={RouteComponent} />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
